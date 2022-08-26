3438addf8524aeff8933c3ea3583672b0f84632a
Anastasiya Chura <a.chura@itransition.com>
2022-08-26 14:43:56 +0400

    add sass, fix bugs

--- calendar/src/App.tsx
+++ calendar/src/App.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import './App.css';
-import { CalendarPage } from './components/Calendar/CalendarPage.tsx';
-import { BrowserRouter } from 'react-router-dom';
-
+import React from "react";
+import "./App.scss";
+import { CalendarPage } from "./components/Calendar/CalendarPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <div className="container">
       <CalendarPage />
-    </BrowserRouter>
+    </div>
   );
 }
 
---
+++ calendar/src/useContentful.tsx
@@ -0,0 +1,42 @@
+interface BirthdayItem {
+  name: string;
+  date: string;
+}
+
+const query = `
+{
+  birthdaysCollection {
+    items {
+      name,
+      date
+    }
+  }
+}`;
+
+export const useContentful = () => {
+  const getBirthdays = async (): Promise<BirthdayItem[]> => {
+    try {
+      return fetch(
+        `https://graphql.contentful.com/content/v1/spaces/g5vcvjt0rgq7/`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer L482r6IOCJjaooW7x2WDVsyi4XOVy5cHWlFmINREF9w",
+          },
+          body: JSON.stringify({ query }),
+        }
+      )
+        .then((response) => response.json())
+        .then(({ data, error }) => {
+          if (error) {
+            throw new Error("Error fetching birthdays");
+          }
+          return data.birthdaysCollection.items;
+        });
+    } catch (error) {
+      reportError({ message: error.message });
+    }
+  };
+  return { getBirthdays };
+};
